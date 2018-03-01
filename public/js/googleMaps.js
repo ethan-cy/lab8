ccaeb642e8f1edeb572777d9f3107846b305cfba
Yi Cai <yic305@ucsd.edu>
2018-03-01 10:42:05 -0800

    Lab 8

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	var ucsd_itlng = {lat:32.880, lng:-117.236}
+
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: ucsd_itlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_itlng,
+		map: map,
+		title:"UCSD"
+	});
 }
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,9 +18,29 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function(){
+		$(".judge-image").bind("taphold", tapholdHandler);
+
+		function tabholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	})
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+  	e.preventDefault();
+  	console.log("submitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+  });
+
+  function postCallback(res) {
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val('');
+  }
 }
